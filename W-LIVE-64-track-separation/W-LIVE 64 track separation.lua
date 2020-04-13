b4f7e73cd1365accc73958128bf645b765f78527
dom <dja@domi.co.uk>
2020-04-13 14:24:12 +0200

    add track renaming using table of names

--- W-LIVE-64-track-separation/W-LIVE 64 track separation.lua
+++ W-LIVE-64-track-separation/W-LIVE 64 track separation.lua
@@ -19,7 +19,12 @@ function chmode(tr, cm)
 	-- deselect track:
 	reaper.SetTrackSelected(track, false)
 end
-
+function renametrack(tr, name)
+	-- get track and select it:
+	track = reaper.GetTrack(0, tr - 1)
+	-- rename track to name param:
+	reaper.GetSetMediaTrackInfo_String(track, "P_NAME", name, true)
+end
 -- loop duplication, to make a track for each channel:
 for i = 0, 4 do
 	reaper.Main_OnCommand(40296, 0)
@@ -44,4 +49,73 @@ for t = 35, 64 do
 	-- same as SD A, except subtract 32 from track number to get the correct command:
 	-- [I've left this so verbose (instead of just t - 41355) so that it's clear what is going on]
 	chmode(t, t - 1 + 41386 - 32)
+end
+channels = {
+	'Kick',
+	'Snare',
+	'Clap',
+	'HiHat',
+	'MFB522 L',
+	'MFB522 R',
+	'Minilogue',
+	'MicroBrute',
+	'Volca Bass',
+	'Volca Keys',
+	'Volca Sample L',
+	'Volca Sample R',
+	'Volca FM',
+	'Anode',
+	'D20 L',
+	'D20 R',
+	'DK-80',
+	'',
+	'K4r L',
+	'K4r R',
+	'Patchblock L',
+	'Patchblock R',
+	'Drum bus L',
+	'Drum bus R',
+	'Delay 1 L',
+	'Delay 1 R',
+	'Reverb 1 L',
+	'Reverb 1 R',
+	'Reverb 2 L',
+	'Reverb 2 R',
+	'Bus 4 L',
+	'Bus 4 R',
+	'Kick pre',
+	'Snare pre',
+	'Clap pre',
+	'HiHat pre',
+	'MFB522 L pre',
+	'MFB522 R pre',
+	'Patchblock L pre',
+	'Patchblock R pre',
+	'MicroBrute pre',
+	'Volca Bass pre',
+	'Volca FM pre',
+	'Volca Keys pre',
+	'Volca Sample L pre',
+	'Volca Sample R pre',
+	'Anode pre',
+	'Minilogue pre',
+	'D20 L pre',
+	'D20 R pre',
+	'DK-80 pre',
+	'',
+	'K4r L pre',
+	'K4r R pre',
+	'Zoom B3n L pre',
+	'Zoom B3n R pre',
+	'',
+	'',
+	'',
+	'',
+	'',
+	'',
+	'Main 3 L',
+	'Main 3 R',
+}
+for i = 1, 64 do
+	renametrack(i, channels[i])
 end
